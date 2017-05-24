2fa0e8d0995fb02c794f2110c1103a38d6276525
chaves04 <emerson04081997@gmail.com>
2017-05-24 15:50:06 -0300

    Update Agenda Eletrônica.c

--- Agenda Eletrônica.c
+++ Agenda Eletrônica.c
@@ -1,41 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-
-typedef struct agenda{
-    char nome[30];
-    char telefone[11];
-    char endereço[30];
-
-    struct agenda *prox;
-}agenda;
-
-struct agenda *aloca(){
-    return malloc(sizeof(agenda));
-}
-
-void InsereNome (agenda *cabeca){
-    agenda *aux, *novo;
-
-    aux = cabeca;
-
-    while(aux->prox != NULL){
-        aux = aux->prox;
+ 
+typedef struct Agenda{
+    char x[30];
+    char y[30];
+    int z[10];
+    struct agenda *proximo;
+}data_agenda;
+ 
+int main(int argc, char *arqv[]){
+    data_agenda *ini_agenda;
+    data_agenda *proximo_agenda;
+    int resp;
+ 
+    ini_agenda = (data_agenda *)malloc(sizeof(data_agenda));
+    if(ini_agenda == NULL)
+        exit(1);
+    proximo_agenda = ini_agenda;
+    while(1)
+    {
+ 
+            printf("Digite o nome: ");
+            scanf("%[^\n]s", &proximo_agenda->x);
+            getchar();
+ 
+            printf("Digite o endereco: ");
+            scanf("%[^\n]s", &proximo_agenda->y);
+           	
+ 
+            printf("Digite o telefone: ");
+            scanf("%s", &proximo_agenda->z);
+ 
+            printf("Deseja continua? <1> <outro valor> NAO: ");
+            scanf("%d", &resp);
+ 
+            if(resp == 1)
+            {
+                proximo_agenda->proximo = (data_agenda *)malloc(sizeof(data_agenda));
+                proximo_agenda = proximo_agenda->proximo;
+            }
+            else
+                break;
+ 
     }
-    novo = aloca();
-
-    printf("Digite o nome: ");
-    gets (novo->nome);
-    novo->prox = NULL;
-
-    aux->prox = novo;
-};
-
-int main(){
-    agenda acervo;
-    acervo.prox = NULL;
-
-    InsereNome (&acervo);
-    printf("%s\n",acervo.prox->nome);
-
+            printf("\n");
+            proximo_agenda->proximo = NULL;
+            proximo_agenda = ini_agenda;
+            while(proximo_agenda != NULL)
+            {
+                    printf("nome: %s, endereco: %s, telefone: %s", proximo_agenda->x, proximo_agenda->y, proximo_agenda->z);
+                    proximo_agenda = proximo_agenda->proximo;
+            }
+ 
+ return 0;
 }
+ 
+ 
